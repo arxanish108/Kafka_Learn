8adc960dbbfe863f5506e66d2b4593fb690ac89c
anish <rowdyroshan108@gmail.com>
2024-02-09 16:40:38 +0530

    kafka

---
+++ src/main/java/com/deliveryBoy/deliverBoy/DeliverBoyApplication.java
@@ -0,0 +1,13 @@
+package com.deliveryBoy.deliverBoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverBoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverBoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliveryBoy/deliverBoy/DeliverBoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliveryBoy.deliverBoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverBoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
